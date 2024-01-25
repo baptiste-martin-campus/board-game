270c48aea575f1336da161b0627a5dc4f9c204df
Coulibaly <ibrahima.coulibaly@le-campus-numerique.fr>
2024-01-25 16:46:08 +0100

    board TicTacToe et Connect4

--- src/fr/lecampusnumerique/cda25/javaalgo/squaregame/board/Connect4Board.java
+++ src/fr/lecampusnumerique/cda25/javaalgo/squaregame/board/Connect4Board.java
@@ -1,4 +1,19 @@
 package fr.lecampusnumerique.cda25.javaalgo.squaregame.board;
 
+import fr.lecampusnumerique.cda25.javaalgo.squaregame.cell.Connect4Cell;
+import fr.lecampusnumerique.cda25.javaalgo.squaregame.cell.TicTacToeCell;
+
 public class Connect4Board {
+    private static final int ranger = 6;
+    private static final int colonne = 7;
+    private Connect4Cell[][] board;
+
+    public Connect4Board(){
+        board = new Connect4Cell[ranger][colonne];
+        for (int i = 0; i < ranger; i++) {
+            for (int j = 0; j < colonne; j++) {
+                board[i][j] = new Connect4Cell();
+            }
+        }
+    }
 }
--- src/fr/lecampusnumerique/cda25/javaalgo/squaregame/board/TicTacToeBoard.java
+++ src/fr/lecampusnumerique/cda25/javaalgo/squaregame/board/TicTacToeBoard.java
@@ -1,4 +1,18 @@
 package fr.lecampusnumerique.cda25.javaalgo.squaregame.board;
 
+import fr.lecampusnumerique.cda25.javaalgo.squaregame.cell.TicTacToeCell;
+
 public class TicTacToeBoard extends Board{
+    private static final int SIZE = 3;
+    private TicTacToeCell[][] board;
+
+    public TicTacToeBoard(){
+        board = new TicTacToeCell[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = new TicTacToeCell();
+            }
+        }
+    }
+
 }
