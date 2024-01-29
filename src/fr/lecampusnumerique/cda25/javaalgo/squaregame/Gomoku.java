c4990a112af41defadd2217cdce38205b3b69da8
R.MARTIN <remy.martin@le-campus-numerique.fr>
2024-01-29 16:07:38 +0100

    Refactor : Supprime les classes board superflue

--- src/fr/lecampusnumerique/cda25/javaalgo/squaregame/Gomoku.java
+++
@@ -1,4 +0,0 @@
-package fr.lecampusnumerique.cda25.javaalgo.squaregame;
-
-public class Gomoku extends BoardGame {
-}
--- src/fr/lecampusnumerique/cda25/javaalgo/squaregame/TicTacToe.java
+++
@@ -1,5 +0,0 @@
-package fr.lecampusnumerique.cda25.javaalgo.squaregame;
-
-public class TicTacToe extends BoardGame {
-
-}
