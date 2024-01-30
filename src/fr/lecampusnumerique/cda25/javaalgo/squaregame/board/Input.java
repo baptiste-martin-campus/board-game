122a7ef1e2272866f178acbe9c92149899d7ba45
Coulibaly <ibrahima.coulibaly@le-campus-numerique.fr>
2024-01-30 09:36:32 +0100

    refact Input

--- src/fr/lecampusnumerique/cda25/javaalgo/squaregame/board/Input.java
+++ src/fr/lecampusnumerique/cda25/javaalgo/squaregame/board/Input.java
@@ -12,16 +12,12 @@ public class Input {
 
     public int coordonateAbs(){
         Scanner first = new Scanner(System.in);
-        view.displayRowChoice();
         return first.nextInt();
     }
-
     public int coordonateOrd(){
         Scanner second = new Scanner(System.in);
-        view.displayColChoice();
         return second.nextInt();
     }
-
     public boolean isHumanPlayer(){
         Scanner choice = new Scanner(System.in);
         String str = choice.nextLine();
